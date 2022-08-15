a43f4f36aa5b0335350f820b41ddb41ed8545e81
Salladen <Salladen@users.noreply.github.com>
2022-08-16 00:05:38 +0200

    Added a method called 'pick' for the 'pick_word' class that other classes can, and should use to get the closest word in the corpus that the AI has for any given word.
    Updated the regexString files.
    
    Created a new class which will replace words in the training set and replace the words which aren't in the corpus to the ones that pick_word.pick() returns.

--- src/RawData_2_TrainingData.java
+++ src/RawData_2_TrainingData.java
@@ -1,2 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class RawData_2_TrainingData {
+    public static void main(String[] args) throws IOException {
+        Scanner reader;
+        File wordlist_file;
+        File regString_file;
+        // Read file that contains the AI's Swedish corpus
+        try {
+            wordlist_file = new File("Data/swe_wordlist.txt");
+            regString_file = new File("Data/regexString");
+            reader = new Scanner(wordlist_file);
+        }
+        catch (FileNotFoundException | NullPointerException e) {
+            throw new IOException();
+        }
+
+        // Add all the words to a list of strings
+        ArrayList<String> wordlist = new ArrayList<>(430000);
+        while (reader.hasNextLine()) {
+            wordlist.add(reader.nextLine());
+        }
+        Comparator<String> cmp = Comparator
+                .comparing(String::length).reversed();
+        wordlist.sort(cmp);
+
+        reader = new Scanner(regString_file);
+
+        String regexString = reader.nextLine();
+
+        Pattern regPattern = Pattern.compile(regexString, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CHARACTER_CLASS);
+        for (var match : regPattern.matcher("satellit ungarnas måne springer").results().toList()){
+            System.out.println("satellit ungarnas måne springer".substring(match.start(), match.end()));
+        }
+    }
 }
