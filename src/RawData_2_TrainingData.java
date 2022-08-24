a443fc93d7e24bbc584c80f91a2d6a60d7d7b1e2
Salladen <Salladen@users.noreply.github.com>
2022-08-24 16:55:47 +0200

    Started a class that is responsible for converting the raw training data into one a token based NLP AI can use.

--- src/RawData_2_TrainingData.java
+++ src/RawData_2_TrainingData.java
@@ -11,10 +11,13 @@ public class RawData_2_TrainingData {
         Scanner reader;
         File wordlist_file;
         File regString_file;
+        File song_file;
+
         // Read file that contains the AI's Swedish corpus
         try {
-            wordlist_file = new File("Data/swe_wordlist.txt");
+            wordlist_file = new File("Data/swe_wordlist_v2.txt");
             regString_file = new File("Data/regexString");
+            song_file = new File("Data/Songs/1.txt");
             reader = new Scanner(wordlist_file);
         }
         catch (FileNotFoundException | NullPointerException e) {
@@ -23,6 +26,8 @@ public class RawData_2_TrainingData {
 
         // Add all the words to a list of strings
         ArrayList<String> wordlist = new ArrayList<>(430000);
+        wordlist.add(" ");
+        wordlist.add("\n");
         while (reader.hasNextLine()) {
             wordlist.add(reader.nextLine());
         }
@@ -33,10 +38,27 @@ public class RawData_2_TrainingData {
         reader = new Scanner(regString_file);
 
         String regexString = reader.nextLine();
-
         Pattern regPattern = Pattern.compile(regexString, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CHARACTER_CLASS);
-        for (var match : regPattern.matcher("satellit ungarnas måne springer").results().toList()){
-            System.out.println("satellit ungarnas måne springer".substring(match.start(), match.end()));
+
+        reader = new Scanner(song_file);
+        ArrayList<String> songLines = new ArrayList<String>();
+
+        while (reader.hasNextLine()){
+            songLines.add(reader.nextLine());
+        }
+        songLines.remove(0);
+        songLines.remove(0);
+        songLines.remove(0);
+        String song = String.join("\n", songLines);
+
+        for (var match : regPattern.matcher(song).results().toList()) {
+            String matchString = song.substring(match.start(), match.end()).toLowerCase();
+
+            if (!wordlist.contains(matchString)) {
+                System.out.printf("Match: [%s]\n", matchString);
+                System.out.println("----NOT IN CORPUS----");
+                System.out.printf("Replacement: [%s]\n", pick_word.pick(wordlist, matchString));
+            }
         }
     }
 }
