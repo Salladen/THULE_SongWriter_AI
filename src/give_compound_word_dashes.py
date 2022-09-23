7ef399f38b050cad2700dcb5c122bc4cb36b9645
Salladen <Salladen@users.noreply.github.com>
2022-09-23 13:48:39 +0200

    Still working on splitting words

--- src/give_compound_word_dashes.py
+++ src/give_compound_word_dashes.py
@@ -1,5 +1,17 @@
 import re, os, string
 import sys
+from textdistance import Levenshtein, RatcliffObershelp
+
+
+def choose_split(origin_word, split_suggestions: list[str]):
+    split_suggestions = [
+        (sum([RatcliffObershelp.similarity(origin_word, part) for part in compound.split("-")]), compound)
+        for compound in split_suggestions
+    ]
+    split_suggestions.sort(key= lambda x: 1 - x[0])
+    print(split_suggestions)
+
+    return split_suggestions[0][1]
 
 
 def main():
@@ -22,8 +34,13 @@ def main():
         words_2_index = dict(zip(words, range(len(words))))
 
     words_escapes = [word for word in words if word[0] in string.ascii_letters ]
+    words_escapes_reverse = reversed(words_escapes.copy())
+
     reg_str = re.compile(
         fr'({"|".join(words_escapes)})', re.UNICODE
+    )
+    reg_str_reversed = re.compile(
+        fr'({"|".join(words_escapes_reverse)})', re.UNICODE
     )
 
     song_amount = len([name for name in os.listdir(songs_path) if not os.path.isdir(name)])
@@ -41,7 +58,9 @@ def main():
             if not match[0] in words:
                 print(match)
 
-                with_dash = "-".join((m[0] for m in reg_str.finditer(match[0])))
+                with_dash = choose_split(match[0],
+                                         ["-".join((m[0] for m in reg_str.finditer(match[0]))),
+                                          "-".join((m[0] for m in reg_str_reversed.finditer(match[0])))])
                 song_text = song_text.replace(match[0], with_dash)
 
         with open(f"{f.name[:7]}/Song_Compound_Dashes/{f.name[14:]}", "w", encoding="UTF-8") as g:
